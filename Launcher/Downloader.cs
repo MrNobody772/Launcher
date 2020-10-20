9894424b584c5bdd802e113e733e4159d180f8bb
pan7nikt <mi.3@wp.pl>
2020-10-20 11:40:53 +0200

    Dodaj pliki projektów.

---
+++ Launcher/Downloader.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Launcher
+{
+    class Downloader
+    {
+        public void main() 
+        {
+            Console.WriteLine("Pobieranie uruchomione sex");
+        }
+    }
+}
---
+++ Launcher/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Security.Policy;
+
+namespace Launcher
+{
+
+    class Program
+    {
+        private WebClient wc = new WebClient();
+        //static Program tenprogram = new Program();
+        static Downloader tendownloader = new Downloader();
+
+        Uri url = null;
+
+        static void Main()
+        {
+            //Console.WriteLine("Hello World!");
+            tendownloader.main();
+
+            //tenprogram.wc.DownloadFileAsync(new System.Uri("https://dl.dropboxusercontent.com/s/ya6m3l3mdrqfd3h/tekstowy.TXT"), "downloaded1.txt");
+
+            //pr.wc.DownloadFileAsync(new System.Uri("https://dl.dropboxusercontent.com/s/ya6m3l3mdrqfd3h/tekstowy.TXT"), "downloaded1.txt");
+        }
+
+    }
+}
